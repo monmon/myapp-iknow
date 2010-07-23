820d49ee41bb855d85981675e7015cb455cf5025
monmon <lesamoureuses@gmail.com>
2010-07-23 12:58:09 +0900

    initial commit

---
+++ lib/MyApp/iKnow.pm
@@ -0,0 +1,32 @@
+package MyApp::iKnow;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MyApp::iKnow -
+
+=head1 SYNOPSIS
+
+  use MyApp::iKnow;
+
+=head1 DESCRIPTION
+
+MyApp::iKnow is
+
+=head1 AUTHOR
+
+monmon E<lt>noE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
